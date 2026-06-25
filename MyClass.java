a5a643d43b893b38272d071085ac9bf4118aed38
zuberiyaanees <aneeszuberiya@gmail.com>
2026-06-26 00:32:38 +0530

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,11 @@
+class MyClass{
+public void sayHello(String name){
+System.out.println("hello" + name);
+}
+}
+class ClassDemo1{
+public static void main(String[] args){
+MyClass m=new MyClass();
+m.sayHello("zuberiya anees");
+}
+}
